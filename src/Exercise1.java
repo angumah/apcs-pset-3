9150ab7b9b3b5b998d1a4890b6ac4e50ac3a3e9b
angumah <tonsterngumah@gmail.com>
2020-09-27 19:49:03 -0400

    final

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,5 +1,5 @@
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println("H E L L O\nE       A\nL       C\nL       P\nO A C P S");
+        System.out.println("H E L L O\nE       A\nL       P\nL       C\nO A P C S");
     }
 }
